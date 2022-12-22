7f808980a3a73baa720723ed3cb82e54933b733e
RitwikKapoor <kapoorritwik2020@gmail.com>
2022-12-22 12:53:26 +0530

    app.use added

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
 const dotenv = require('dotenv')
+const path = require('path')
 
 dotenv.config({path:"backend/config/config.env"})
 
@@ -26,6 +27,11 @@ app.use("/api/v1",product);
 app.use("/api/v1",user);
 app.use("/api/v1",order);
 app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+})
 
 //middleware for errors
 
